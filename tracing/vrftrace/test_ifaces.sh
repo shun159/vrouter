1366aaded8187723af12b05eb80dea81476ac6f7
Eishun Kondoh <shun3382@gmail.com>
2022-01-27 13:12:18 +0900

    tracing/bpf: Fix demo iface configuration
    Change-Id: I6e80a3c0171f56d0fe5776864368a4ef845c1516

--- tracing/vrftrace/test_ifaces.sh
+++ tracing/vrftrace/test_ifaces.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-#echo "1 1" > /sys/bus/netdevsim/new_device 2>/dev/null
 ip link add dev pkt0  type dummy
-ip link add dev fabric0 type veth peer name veth0
-ip link add dev tap0 type veth peer name veth1
-ip link add dev tap1 type veth peer name veth2
+ip link add dev tap0 type veth peer name veth0
+ip link add dev tap1 type veth peer name veth1
 
 ip link set dev pkt0 up
-ip link set dev fabric0 up
 ip link set dev tap0 up
 ip link set dev tap1 up
 ip link set dev veth0 up
 ip link set dev veth1 up
-ip link set dev veth2 up
